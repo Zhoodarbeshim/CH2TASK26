28bcf23b43dae141e14c8d3431145087dcdab4ef
Zhoodarbeshim <zh.bektursunov@yandex.com>
2019-12-30 09:49:28 +0600

    twenty sixth commit

---
+++ task26.py
@@ -0,0 +1,6 @@
+even_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+even_num = [x for x in even_num if x % 2 == 0]
+print(even_num)
+odd_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+odd_num = [x for x in odd_num if x % 2 == 1]
+print(odd_num)
